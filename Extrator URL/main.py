ab85788fcf42e97c6f58c34235e6c239812b5d3b
José Luciano de Oliveira Filho <60415276+Romantictrash@users.noreply.github.com>
2023-07-20 11:44:31 -0300

    estudando strings

---
+++ Extrator URL/main.py
@@ -0,0 +1,20 @@
+def filtra_valor (dado):
+    dado_index = dado.find('=')
+    dado_valor = dado[dado_index + 1 :]
+    return dado_valor
+
+url = "https://bytebank.com/cambio?moedaOritem=real&moedaDestino=dolar"
+dados = url.split("?")
+url_base = dados[0]
+url_parametros = dados[1]
+
+parametros = url_parametros.split("&")
+primeiro_parametro = parametros[0]
+primeiro_parametro_valor = filtra_valor(primeiro_parametro)
+
+segundo_parametro = parametros[1]
+segundo_parametro_valor = filtra_valor(segundo_parametro)
+
+print(url)
+print(url_base)
+print(f"primeiro parametro tem o valor de {primeiro_parametro_valor} e o segundo parametro tem o valor de {segundo_parametro_valor}")
